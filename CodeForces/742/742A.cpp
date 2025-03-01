24ac4c767dc2c5d74b6c3d542fb896a2ea710172
RHJihan <31377578+RHJihan@users.noreply.github.com>
2025-03-01 08:11:37 +0600

    add codeforces 742

---
+++ CodeForces/742/742A.cpp
@@ -0,0 +1,25 @@
+// https : // codeforces.com/problemset/problem/742/A
+#include <iostream>
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+  (void)!freopen("/home/jihan/Documents/problem-solving/input.txt", "r", stdin);
+  // (void)!freopen("/home/jihan/Documents/problem-solving/output.txt","w", stdout);
+#endif
+
+  long long n;
+  cin >> n;
+
+  if (n == 0) {
+    cout << 1 << endl;
+    return 0;
+  }
+
+  int last_digits[] = {8, 4, 2, 6};
+  int index = (n - 1) % 4;
+
+  cout << last_digits[index] << endl;
+
+  return 0;
+}
