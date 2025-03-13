273c6b60dff47b38c26ba30099f8230f538ad642
RHJihan <31377578+RHJihan@users.noreply.github.com>
2025-03-13 22:18:35 +0600

    add UVA 429

---
+++ UVA/429-Word_Transformation.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+  (void)!freopen("/home/jihan/Documents/problem-solving/input.txt", "r", stdin);
+//   (void)!freopen("/home/jihan/Documents/problem-solving/output.txt","w", stdout);
+#endif
+
+  int test;
+  cin >> test;
+  while (test--) {
+    set<string> words;
+    string word;
+    while (cin >> word) {
+      if (word == "*") {
+        break;
+      }
+      words.insert(word);
+    }
+
+    cin.ignore();
+
+    string line;
+    while (getline(cin, line)) {
+      if (line.empty()) {
+        break;
+      }
+      stringstream ss(line);
+      string s, t;
+      ss >> s >> t;
+
+      queue<string> q;
+      map<string, int> transformation;
+
+      transformation[s] = 0;
+      q.push(s);
+      bool found = false;
+
+      while (!q.empty() && !found) {
+        string u = q.front();
+        q.pop();
+
+        for (auto it = words.begin(); it != words.end(); it++) {
+          const string &current = *it;
+          if (!transformation.count(current) && u.length() == current.length()) {
+            int diff = 0;
+            for (size_t j = 0; j < u.length(); ++j)
+              if (u[j] != current[j])
+                diff++;
+
+            if (diff == 1) {
+              transformation[current] = transformation[u] + 1;
+              q.push(current);
+              if (current == t) {
+                found = true;
+                break;
+              }
+            }
+          }
+        }
+      }
+
+      cout << s << " " << t << " " << transformation[t] << endl;
+    }
+    if (test != 0) {
+      cout << endl;
+    }
+  }
+  return 0;
+}
