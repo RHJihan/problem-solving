2a68990878412cf9184e116063c3d699ce3b7589
RHJihan <31377578+RHJihan@users.noreply.github.com>
2025-03-19 13:39:44 +0600

    add codeforces 1216

---
+++ CodeForces/1216/1216A.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+  (void)!freopen("/home/jihan/Documents/problem-solving/input.txt", "r", stdin);
+  // (void)!freopen("/home/jihan/Documents/problem-solving/output.txt","w", stdout);
+#endif
+
+  int n;
+  string s;
+  cin >> n >> s;
+
+  int count = 0;
+
+  for (int i = 0; i < n; i += 2) {
+    int a_count = (s[i] == 'a') + (s[i + 1] == 'a');
+    int b_count = 2 - a_count;
+
+    if (a_count != b_count) {
+      if (a_count > b_count) {
+        s[i + 1] = 'b';
+      } else {
+        s[i] = 'a';
+      }
+      count++;
+    }
+  }
+
+  cout << count << endl;
+  cout << s << endl;
+
+  return 0;
+}
---
+++ CodeForces/1216/1216B.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+  (void)!freopen("/home/jihan/Documents/problem-solving/input.txt", "r", stdin);
+  // (void)!freopen("/home/jihan/Documents/problem-solving/output.txt","w", stdout);
+#endif
+
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+
+  vector<pair<int, int>> cans(n);
+  for (int i = 0; i < n; i++) {
+    cans[i] = {a[i], i + 1};
+  }
+
+  sort(cans.rbegin(), cans.rend());
+
+  int total = 0;
+  vector<int> order;
+  for (int i = 0; i < n; i++) {
+    int current = cans[i].first * i + 1;
+    total += current;
+    order.push_back(cans[i].second);
+  }
+
+  cout << total << endl;
+  for (int i = 0; i < n; i++) {
+    cout << order[i] << " ";
+  }
+  cout << endl;
+  return 0;
+}
