91c1e38185a35aacf7e5bb1f8d0998f8864bdc6c
RHJihan <31377578+RHJihan@users.noreply.github.com>
2025-03-20 12:53:55 +0600

    add codeforces 1154

---
+++ CodeForces/1154/1154A.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+  (void)!freopen("/home/jihan/Documents/problem-solving/input.txt", "r", stdin);
+  // (void)!freopen("/home/jihan/Documents/problem-solving/output.txt","w", stdout);
+#endif
+
+  vector<int> nums(4);
+  for (int i = 0; i < 4; i++) {
+    cin >> nums[i];
+  }
+
+  sort(nums.begin(), nums.end());
+
+  cout << nums[3] - nums[2] << " " << nums[3] - nums[1] << " " << nums[3] - nums[0] << endl;
+
+  return 0;
+}
