90c2c67e75be8c4882d8fa7a0406f06a61f54430
RHJihan <31377578+RHJihan@users.noreply.github.com>
2025-03-30 22:54:24 +0600

    add codeforces 1141

---
+++ CodeForces/1141/1141A.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+  (void)!freopen("/home/jihan/Documents/problem-solving/input.txt", "r", stdin);
+  // (void)!freopen("/home/jihan/Documents/problem-solving/output.txt","w", stdout);
+#endif
+
+  int n, m;
+  cin >> n >> m;
+
+  if (m % n != 0) {
+    cout << -1 << endl;
+  } else {
+    int moves = 0;
+    int ratio = m / n;
+
+    while (ratio % 2 == 0) {
+      ratio /= 2;
+      moves++;
+    }
+    while (ratio % 3 == 0) {
+      ratio /= 3;
+      moves++;
+    }
+    cout << ((ratio == 1) ? moves : -1) << endl;
+  }
+  return 0;
+}
