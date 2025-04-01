0dcd714551f894a0d02bb2af7bfff04dfdccb2db
RHJihan <31377578+RHJihan@users.noreply.github.com>
2025-04-01 15:15:55 +0600

    add codeforces 1165

---
+++ CodeForces/1165/1165A.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+  (void)!freopen("/home/jihan/Documents/problem-solving/input.txt", "r", stdin);
+  // (void)!freopen("/home/jihan/Documents/problem-solving/output.txt","w", stdout);
+#endif
+
+  int n, x, y;
+  cin >> n >> x >> y;
+
+  string number;
+  cin >> number;
+
+  int count = 0;
+
+  for (int i = n - x; i < n; i++) {
+    if (i == n - y - 1) {
+      if (number[i] != '1') {
+        count++;
+      }
+    } else {
+      if (number[i] != '0') {
+        count++;
+      }
+    }
+  }
+
+  cout << count << endl;
+  return 0;
+}
