060e7d972790b335960968c448f6560559b5d502
Md. Rifat Hasan Jihan <31377578+RHJihan@users.noreply.github.com>
2025-12-24 17:23:18 +0600

    Add SQL solutions for LeetCode problems: Combine Two Tables, Second Highest Salary, Nth Highest Salary, Employees Earning More Than Their Managers, Duplicate Emails, and Customers Who Never Order

---
+++ LeetCode/177-nth-highest-salary.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS $$
+BEGIN
+  IF N <= 0 THEN
+    RETURN QUERY SELECT NULL::INT AS Salary;
+  ELSE
+    RETURN QUERY
+    SELECT DISTINCT e.salary
+    FROM Employee e
+    ORDER BY e.salary DESC
+    OFFSET N - 1 LIMIT 1;
+  END IF;
+END;
+$$ LANGUAGE plpgsql;
